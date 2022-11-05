ed0386a901f39d4abef0b6fe08f73fb607f0d26b
william-franco <williamff@outlook.com>
2022-11-05 17:35:37 -0300

    update dart sdk and libs: go_router, flutter_riverpod and fix error theme state

--- lib/src/widgets/molecules/app_bar_molecule.dart
+++ lib/src/widgets/molecules/app_bar_molecule.dart
@@ -5,7 +5,6 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 class AppBarMolecule extends StatelessWidget implements PreferredSizeWidget {
-  // final bool isBack;
   final Widget title;
   final List<Widget>? actions;
 
